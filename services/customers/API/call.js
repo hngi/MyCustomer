8e282c37992f3496ed6cea64e0fad624b4f51620
Tobi Ajibade <herityjohnny14>
2020-06-18 14:47:35 -0700

    phone calling  api added

--- services/customers/API/call.js
+++ services/customers/API/call.js
@@ -5,6 +5,8 @@ const app = express();
 
 const router = express.Router();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 const accountSid = process.env.TWILIO_SID;
 const auth = process.env.TWILIO_AUTH;
@@ -12,14 +14,25 @@ const twilioClient  = require('twilio')(accountSid,auth);
 
 
     //Twilio call service
-router.post(/api/call, (req,res) => {
+router.post('/v1/call', (req,res) => {
+
+    /*seen the number format required is (+234),  so the incoming number has to be
+        filtered down to the required format
+        e.g from 080000000 to +23480000000
+        */
+    //filtering
+        const incomingNum = req.body.number;
+        const toString = String(incomingNum);
+        const slicedNum = toString.slice(1);
+        const fullNumber = `+234${slicedNum}`;
+
     twilioClient.calls.create({
         url : 'https://handler.twilio.com/twiml/EHa97c46232b3eddd7a779694fbc60a69d',
-        to : '+2348034051060',
+        to : fullNumber,
         from : process.env.TWILIO_NUMBER
     })
-    .then(calls => console.log(calls.sid + ' Calling now!'))
-    .catch(err => console.log(err));
+    .then(calls => res.json(calls))
+    .catch(err => res.json(err))
 })
    
 
